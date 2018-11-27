2edef8f419f497f01bcbd5ed45787d7e6d3dc9bd
J. Johnson <opticsdeveloper@gmail.com>
2018-11-27 06:32:43 -0500

    Updated Functions for calculating slope. Refactored portions of the slope funcitons. Added Comments

--- deflectometer/deflectometer.py
+++ deflectometer/deflectometer.py
@@ -32,6 +32,7 @@ class Camera():
             self.dist_map = self.distortion_map(dist_map)
         else:
             self.dist_map = np.ones(self.shape)
+        self.calc_noise()
 
     def poll_cam_meta(self):
         """Returns a dict of the current base camera properties"""
@@ -48,14 +49,19 @@ class Camera():
 
     def ifov(self, ffov=30, orientation='width'):
         """ function to add ifov to cam object. Needed to cal part location"""
-        ffov *= np.pi/180  # convert to radians
+        ffov *= np.pi / 180  # convert to radians
         self.poll_cam_meta()
         if orientation == 'width':
             self.ifov = ffov / self.shape[1]
         elif orientation == 'height':
             self.ifov = ffov / self.shape[0]
+        xang = (np.arange(0, self.shape[1]) - self.shape[1] / 2) * self.ifov
+        yang = (np.linspace(0, self.shape[0]) - self.shape[1] / 2) * self.ifov
+        self.fov_array = np.meshgrid(xang, yang)
 
-
+    def calc_noise(self, nframes=30):
+        """ Collect dark noise for camera """
+        self.avg_noise = 10
 
 class Part(object):
     """basic part properties"""
@@ -96,31 +102,73 @@ def create_window():
 #     return cam_obj
 
 
+def calc_separation(fig, ar, cam, frame, lit_pix=(0, 0), screen_cam=(-150, 0), part_dist=1000):
+    """units are in mm
+
+    Two lateral separations are required for the deflectomer (lit Screen-lit part, lit part-camera)
+
+    --
+
+    Math:
+    L = distance from cam to screen
+    Z = axial distance between the part and the camera-screen aparatus
+    IFOV = Camera IFOV
+    campix = lit camera pixel
+    centerpix = central pixel that notes boresight of the camera ie angle of zero-zero
+
+    theta = IFOV * (campix-centerpix)  -- the angle of the illuminated point on the part as seen by the camera
+    xpc = Z * tan(theta)  -- the linear distance between the camera and the illuminated point on the part
+    xsp = L - xpc  -- the linear distance between the screen's lit pixel and the illuminated point on the part
 
 
-def calc_separation(fig, ar, lit_x=0, lit_y=0, cam_x=0, cam_y=150, ):
-    """units are in mm"""
-    # zero is defined as center(upperleft of quad) screen pixel
+    Coordinate frame notes: it may help to treat the screen as being a negative distance away from the camera with smaller pixels indicies being closer to the camera
+
+
+    Parameters
+    -----------
+    fig : matplotlib.pyplot.figure object
+
+    ar : ndarray (int)  -- pixelated screen projection array
+
+    lit_x : tuple(int) -- lit pixel of screen in (Y, X) i.e. (Row, Col) C-style format
+
+    screen_cam : tuple(int) -- distance from cam to center of screen in form (Y, X). Coordinate frame zero is defined as center of camera 'pinhole'
+
+    Returns
+    -----------
+
+    x_sep : tuple (float) -- 2 term tuple of the x separation arrays of (screen-part, part-cam)
+
+    y_sep : tuple (float) -- 2 term tuple of the y separation arrays of (screen-part, part-cam)
+    """
     # import pdb; pdb.set_trace()
+
+    # find center of screen
     centerx = ar.shape[1] // 2
     centery = ar.shape[0] // 2
-    # pitch_size = 25.4 / fig.dpi  # mm / pix
     screen_size = fig.get_size_inches() * 25.4  # in mm
 
-    screenx_sep = (lit_x - centerx + 0.5) / ar.shape[1] * screen_size[0]
-    screeny_sep = (centery - lit_y - 0.5) / ar.shape[0] * screen_size[1]
+    # Calculate separation of lit pix from camera (L from equations above)
+    screencamx_sep = (lit_pix[1] - centerx) / ar.shape[1] * screen_size[0] + screen_cam[1]
+    screencamy_sep = (centery - lit_pix[0]) / ar.shape[0] * screen_size[1] + screen_cam[0]
+
+    frame_x, frame_y = find_in_frame(cam, frame, threshold=150)
+
+    # Calculate xpc and xsp for the X and Y vectors
+    camx_sep = np.tan(frame_x) * part_dist
+    camy_sep = np.tan(frame_y) * part_dist
+
+    # Calculate xsp for the X and Y vectors and combine into tuple with xpc
+    x_sep = (camx_sep, screencamx_sep - camx_sep)
+    y_sep = (camy_sep, screencamy_sep - camy_sep)
     return x_sep, y_sep
 
-
-def calc_slope(sep, dist=1000):
-    # Calculates slope in rad given a separation (either x or y)
-    return sep / dist
 
 def slope_val(pixloc, partloc, camloc, zscreen_part, zcam_part, partsag=0):
     """ Simplified slope vals... needs update for fast parts!"""
 
-    dscreen_part = np.sqrt(zscreen_part**2 + (pixloc-partloc)**2)
-    dcam_part = np.sqrt(zscreen_part**2 + (camloc-partloc)**2)
+    # dscreen_part = np.sqrt(zscreen_part**2 + (pixloc-partloc)**2)
+    # dcam_part = np.sqrt(zscreen_part**2 + (camloc-partloc)**2)
     t1 = (partloc - pixloc) / zscreen_part
     t2 = (partloc - camloc) / zcam_part
     t3 = 1
@@ -128,7 +176,7 @@ def slope_val(pixloc, partloc, camloc, zscreen_part, zcam_part, partsag=0):
     return (t1 + t2) / (t3 + t4)
 
 
-def pix_slope(slope, frame, avg_noise=10, threshold=150):
+def find_in_frame(cam, frame, threshold=150):
     """
     Calculates points illuminated in frame based on centroiding method
 
@@ -144,14 +192,18 @@ def pix_slope(slope, frame, avg_noise=10, threshold=150):
 
     Returns
     ---------
-    ndarray (float)  -- tuple of 2 ndarrays (row, col) indices
+    angle_ar_x : ndarray (float)  -- array of x angles corresponding to lit distances on part
+
+    angle_ar_y : ndarray (float)  -- array of y angles corresponding to lit distances on part
     """
+
     # remove noise
-    # import pdb; pdb.set_trace()
-    frame[frame < (avg_noise + threshold)] = 0
-    slope_ar = np.zeros(frame.shape)
-    slope_ar[frame > 0] = slope
-    return slope_ar
+    frame[frame < (cam.avg_noise + threshold)] = 0
+    angle_ar = np.zeros(frame.shape)
+    angle_ar[frame > 0] = 1
+    angle_ar_x = angle_ar * cam.fov_array[0]
+    angle_ar_y = angle_ar * cam.fov_array[1]
+    return angle_ar_x, angle_ar_y
 
 def combine_maps(ar1, ar2=None):
     """combines two image arrays into single output. Indicies that have non-zero values are averaged"""
@@ -164,6 +216,7 @@ def combine_maps(ar1, ar2=None):
         return outarray
 
 def scranner(screen_ar, barsize=10, dist=1000, camh=150):
+    """ Primary function for capturing deflectometer data"""
     camfig = plt.figure()
     mgr = plt.get_current_fig_manager()
     mgr.window.setGeometry(900, 30, 320, 240)
